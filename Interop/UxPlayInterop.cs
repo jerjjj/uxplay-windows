d883b040510e4d6b4ad98d9f18b0a98c3bf4cb63
jerjjj <wuzhijun@jerjjj.cn>
2026-06-29 11:21:56 +0800

    revert: rollback native changes, keep only upstream report
    Reverted local additions to libuxplay (overscanned, restrict_clients,
    show_fps_data). Will submit as separate PR to upstream.
    C# interop reverted to match restored native struct layout.
    L10n descriptions restored to 'not yet supported'.

--- Interop/UxPlayInterop.cs
+++ Interop/UxPlayInterop.cs
@@ -94,9 +94,6 @@ public struct UxPlayConfig : IDisposable
     [MarshalAs(UnmanagedType.U1)] public bool ForceSoftwareDecoder;
     public IntPtr MetadataFilenamePtr;
     public IntPtr RecordFilenamePtr;
-    [MarshalAs(UnmanagedType.U1)] public bool Overscanned;
-    [MarshalAs(UnmanagedType.U1)] public bool RestrictClients;
-    [MarshalAs(UnmanagedType.U1)] public bool ShowFpsData;
 
     // Managed alloc tracking (12 string slots)
     IntPtr _a0,_a1,_a2,_a3,_a4,_a5,_a6,_a7,_a8,_a9,_a10,_a11,_a12;
@@ -151,8 +148,7 @@ public struct UxPlayConfig : IDisposable
         HlsSupport = s.HlsSupport; NoHold = s.NoHold;
         TaperVolume = s.TaperVolume; SrgbFix = s.SrgbFix;
         AudioLatency = s.AudioLatency; ResetTimeout = s.ResetTimeout;
-        KeepWindow = !s.NewWindowClosing; ForceSoftwareDecoder = s.ForceSoftwareDecoder;
-        Overscanned = s.Overscanned; RestrictClients = s.RestrictClients; ShowFpsData = s.ShowFpsData;
+        KeepWindow = s.KeepWindow; ForceSoftwareDecoder = s.ForceSoftwareDecoder;
 
         // 数组
         TcpPorts ??= new ushort[3];
